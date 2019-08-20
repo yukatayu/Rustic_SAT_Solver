c1128d06ec894580d17fa87350a29b3528d25278
yukatayu <yukatayu.dev@gmail.com>
2019-08-20 21:48:23 +0900

    add: logic puzzle: HoD

--- tests/usage.cpp
+++ tests/usage.cpp
@@ -5,26 +5,41 @@
 int main(){
 	// prepare
 	/**
-	 * A: !B && !C
-	 * B: !A
-	 * C: !B
-	 */
-	/**
-	 * (!A || !B) && (!A || !C) && (B || C || A)
-	 * (!A || !B) && (B || A)
-	 * (!B || !C) && (C || B)
+	 * A: mB
+	 * B: mD
+	 * C: !mC
+	 * D: !B
+	 * 
 	 */
 	auto clauses = ClauseSet({
 		// A
+		{{ "-a"_L,  "mB"_L }},
+		{{ "-mB"_L,  "a"_L }},
+		// B
+		{{ "-b"_L, "mD"_L }},
+		{{ "-mD"_L, "b"_L }},
+		// C
+		{{ "-c"_L, "-mC"_L }},
+		{{ "mC"_L, "c"_L }},
+		// D
+		{{ "-d"_L, "-b"_L }},
+		{{  "d"_L,  "b"_L }},
+		// a + b + c + d = 1
+		{{ "a"_L, "b"_L, "c"_L, "d"_L }},
 		{{ "-a"_L, "-b"_L }},
 		{{ "-a"_L, "-c"_L }},
-		{{  "b"_L,  "c"_L,  "a"_L }},
-		// B
-		{{ "-a"_L, "-b"_L }},
-		{{  "b"_L,  "a"_L }},
-		// C
+		{{ "-a"_L, "-d"_L }},
 		{{ "-b"_L, "-c"_L }},
-		{{  "c"_L,  "b"_L }}
+		{{ "-b"_L, "-d"_L }},
+		{{ "-c"_L, "-d"_L }},
+		// mA + mB + mC + mD = 1
+		{{ "mA"_L, "mB"_L, "mC"_L, "mD"_L }},
+		{{ "-mA"_L, "-mB"_L }},
+		{{ "-mA"_L, "-mC"_L }},
+		{{ "-mA"_L, "-mD"_L }},
+		{{ "-mB"_L, "-mC"_L }},
+		{{ "-mB"_L, "-mD"_L }},
+		{{ "-mC"_L, "-mD"_L }},
 	});
 	// solve
 	auto [isSAT, conds] = solve(clauses);
